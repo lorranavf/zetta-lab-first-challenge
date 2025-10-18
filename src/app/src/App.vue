d667cba12b120a10f256337392696bff760e8593
Lorrana Flores <lorrana.vf@gmail.com>
2025-10-18 10:38:54 -0300

    feat: adding source files with minimal configuration to start the project implementation

---
+++ src/app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container py-4">
+    <h1>Meu App Vue</h1>
+    <p>Se você vê isto, o app está funcionando</p>
+  </div>
+  <div id="app" class="container py-4">
+  <button @click="count++">
+    Count is: {{ count }}
+  </button>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App',
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container { text-align: center; }
+button {
+  font-weight: bold;
+  align-items: center;
+}
+</style>
+

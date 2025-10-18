6c90a2854c1a4142bc782550b7944c0faacc082b
Lorrana Flores <lorrana.vf@gmail.com>
2025-10-18 11:09:19 -0300

    feat: adding header minimal configurations

--- src/app/src/App.vue
+++ src/app/src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="container py-4">
-    <h1>Meu App Vue</h1>
-    <p>Se você vê isto, o app está funcionando</p>
+  <div id="app" class="d-flex flex-column min-vh-100">
+    <Header />
+    <main class="container py-4 flex-grow-1">
+      <h1>Meu App Vue</h1>
+      <p>Se você vê isto, o app está funcionando</p>
+
+      <div class="text-center my-4">
+        <button class="btn btn-primary" @click="count++">
+          Count is: {{ count }}
+        </button>
+      </div>
+    </main>
   </div>
-  <div id="app" class="container py-4">
-  <button @click="count++">
-    Count is: {{ count }}
-  </button>
-</div>
 </template>
 
 <script lang="ts">
+
+import Header from './components/header.vue'
+
 export default {
   name: 'App',
+  components: { Header },
   data() {
     return {
       count: 0
---
+++ src/app/src/components/header.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">ZettaLab</a>
+      <button 
+      class="navbar-toggler" 
+      type="button" 
+      data-bs-toggle="collapse" 
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+      >
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Sobre</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Contato</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.navbar { margin-bottom: 1rem; }
+</style>

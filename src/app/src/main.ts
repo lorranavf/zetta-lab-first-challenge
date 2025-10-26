ff0c432527905fa1d558008c0fb13a556ef195f3
Lorrana Flores <lorrana.vf@gmail.com>
2025-10-26 11:03:45 -0300

    feat: adding Bearer Check to verify if API key is being loaded to the environment

--- src/app/src/main.ts
+++ src/app/src/main.ts
@@ -1,20 +1,23 @@
-import { createApp } from 'vue'
+
 import App from './App.vue'
+
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 import './components/styles/common.scss'
 
-import { createRouter, createWebHistory } from 'vue-router'
-
 import Items from './components/items.vue'
 import Item from './components/item.vue'
+import Bearer from './components/bearer.vue'
 
 const routes = [
   { path: '/items', component: Items },
   { path: '/item/:id', component: Item },
-  { path: '/', redirect: '/items' }
+  { path: '/bearer', component: Bearer },
+  { path: '/', redirect: '/bearer' }
 ]
 
 const router = createRouter({

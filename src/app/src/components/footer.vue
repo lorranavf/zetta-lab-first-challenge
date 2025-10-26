675cc43e4967e446091c4c7af71d32d3376db66f
Lorrana Flores <lorrana.vf@gmail.com>
2025-10-26 16:29:13 -0300

    refactoring: adding TMDB attribution

--- src/app/src/components/footer.vue
+++ src/app/src/components/footer.vue
@@ -1,9 +1,11 @@
 <template>
   <footer class="bg-light text-center py-3 mt-auto">
-    <div class="container">
+    <div>
       <small>© Zetta Lab</small><br />
       <small>2025</small><br />
       <small>Desenvolvido por Lorrana Flores</small>
+      <br />
+      <small>Este produto utiliza a API TMDB, mas não é endossado ou certificado pelo TMDB.</small>
     </div>
   </footer>
 </template>
